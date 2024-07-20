bc4accc7f9f884fc8b0926d55209aaf36354956d
IlseChan <86861461+IlseChan@users.noreply.github.com>
2024-07-19 19:01:32 -0600

    se agrego form-app

--- FrontIA-scss/src/app/app.routes.ts
+++ FrontIA-scss/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FormsAppsPageComponent } from './modules/aplicaciones/components/form-app/form-app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'new-app', component: FormsAppsPageComponent },
+];
