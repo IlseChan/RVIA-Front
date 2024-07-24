2e1b19f021bba5ff659097de5ec470f114e08ec5
Brandon Jaimes <b1jaimes89@gmail.com>
2024-07-24 16:13:06 -0600

    `Feat` registro de usuarios

--- FrontIA-scss/src/app/modules/auth/components/register/register.component.ts
+++ FrontIA-scss/src/app/modules/auth/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -15,17 +16,19 @@ export class RegisterComponent {
   username: string = '';
   password: string = '';
   confirmPassword: string = '';
+  email: string = '';
   errorMessage: string = '';
 
   @ViewChild('passwordInput', { static: false }) passwordInput!: ElementRef;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   onRegister(): void {
     const trimmedUsernumber = this.usernumber.trim();
     const trimmedUsername = this.username.trim();
     const trimmedPassword = this.password.trim();
     const trimmedConfirmPassword = this.confirmPassword.trim();
+    const trimmedEmail = this.email.trim();
 
     if (!/^\d+$/.test(trimmedUsernumber)) {
       this.errorMessage = 'El número de empleado debe contener solo números';
@@ -37,10 +40,22 @@ export class RegisterComponent {
       return;
     }
 
-    // Simula el éxito del registro
-    alert('Registro simulado exitoso');
-    this.errorMessage = ''; // Limpiar mensaje de error en caso de éxito
-    this.router.navigate(['/auth/login']);  // Navega a la página de login
+    if (!/\S+@\S+\.\S+/.test(trimmedEmail)) {
+      this.errorMessage = 'El correo electrónico no es válido';
+      return;
+    }
+
+    this.authService.register(trimmedUsernumber, trimmedUsername, trimmedPassword, trimmedEmail)
+      .subscribe({
+        next: () => {
+          alert('Registro exitoso');
+          this.errorMessage = ''; // Limpiar mensaje de error en caso de éxito
+          this.router.navigate(['/auth/login']);  // Navega a la página de login
+        },
+        error: (error: Error) => { // Especificar el tipo de error
+          this.errorMessage = error.message;
+        }
+      });
   }
 
   onInputChange(): void {
--- FrontIA-scss/src/app/modules/auth/services/auth.service.ts
+++ FrontIA-scss/src/app/modules/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserLogged } from '../interfaces/userLogged.interface';
-import { catchError, of, tap } from 'rxjs';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 
@@ -15,36 +15,41 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   get userLogged(): UserLogged | null {
-    // return this.currentUser;
-    return {
-      idu_usuario: 1,
-      nom_correo: "angel.magan@coppel.com",
-      numero_empleado: 11,
-      position: {
-        idu_puesto: 1,
-        nom_puesto: "Administrador",
-        // nom_puesto: "Autorizador",
-        // nom_puesto: "Usuario",
-        // nom_puesto: "Invitado",
-      },
-      // token: ''
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzIxODM3NTE3LCJleHAiOjE3MjE4NDQ3MTd9.yXCcChs_qVj60uqciR0nS9K5olE-n3hA0e-pDldRrjU"
-    }
+    return this.currentUser;
+    // return {
+    //   idu_usuario: 1,
+    //   nom_correo: "angel.magan@coppel.com",
+    //   numero_empleado: 11,
+    //   position: {
+    //     idu_puesto: 1,
+    //     nom_puesto: "Administrador",
+    //     // nom_puesto: "Autorizador",
+    //     // nom_puesto: "Usuario",
+    //     // nom_puesto: "Invitado",
+    //   },
+    //   // token: ''
+    //   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzIxODM3NTE3LCJleHAiOjE3MjE4NDQ3MTd9.yXCcChs_qVj60uqciR0nS9K5olE-n3hA0e-pDldRrjU"
+    // }
   }
 
-  register(usernumber: string, username: string, password: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const users = JSON.parse(localStorage.getItem('users') || '[]');
-
-      if (users.find((user: any) => user.usernumber === usernumber)) {
-        reject(new Error('El número de empleado ya está registrado'));
-      } else {
-        users.push({ usernumber, username, password });
-        localStorage.setItem('users', JSON.stringify(users));
-        resolve();
-      }
-    });
+  register(usernumber: string, username: string, password: string, email: string): Observable<void> {
+    const idu_puesto = 4; // Asegurar que idu_puesto siempre es 4
+    return this.http.post<void>(`${this.baseUrl}/auth/register`, {
+      numero_empleado: usernumber,
+      nom_usuario: username,
+      nom_contrasena: password,
+      idu_puesto: idu_puesto,
+      nom_correo: email
+    })
+    .pipe(
+      catchError(this.handleError)
+    );
   }
+
+  private handleError(error: any): Observable<never> {
+    return throwError(error.error || 'Server error');
+  }
+
 
   getUsers(): { usernumber: string, username: string, password: string }[] {
     return JSON.parse(localStorage.getItem('users') || '[]');
