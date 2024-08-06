652f07afb116f96a23d3633c002b564f3969c68f
Brandon Jaimes <b1jaimes89@gmail.com>
2024-08-06 17:05:12 -0600

    `refactor` `styles` Se actualiza layout para padding en responsividad y se eliminan elementos innecesarios en modulo usuarios

--- RVIA-Front/src/app/modules/usuarios/services/usuarios.service.ts
+++ RVIA-Front/src/app/modules/usuarios/services/usuarios.service.ts
@@ -27,7 +27,7 @@ export class UsuariosService {
     return token || null;
   }
 
-  clearDataUsers():void {
+  clearDataUsers(): void {
     this.allUsers.data = [];
     this.allUsers.total = -1;
   }
