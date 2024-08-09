346f4a81c7752f6897bb2329d662fc79d99ce964
Brandon Jaimes <b1jaimes89@gmail.com>
2024-08-09 12:01:59 -0600

    `Feat` Se agregan tag para cada tipo de estado de las aplicaciones para la vista de invitados y usuarios

---
+++ src/app/modules/aplicaciones/pipes/test/status-app-label.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { StatusAppLabelPipe } from './status-app-label.pipe';
+
+describe('StatusAppLabelPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StatusAppLabelPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
