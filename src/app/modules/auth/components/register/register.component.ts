815418e7cba9f05971017cda71bf8c80216bd409
Brandon Jaimes <b1jaimes89@gmail.com>
2024-08-21 11:40:48 -0600

    I `Refactor` Se añade validación solo para correos de dominio dado

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -39,16 +39,16 @@ export class RegisterComponent {
     const trimmedConfirmPassword = this.confirmPassword.trim();
     const trimmedEmail = this.email.trim();
 
-     const usernumberInt = parseInt(trimmedUsernumber, 10);
-     if (!((usernumberInt > 90000000 && usernumberInt <= 99999999) || usernumberInt < 100000000)) {
-       this.errorMessage = 'El número de empleado debe ser mayor a 90000000 y menor o igual a 99999999, o menor a 100000000';
-       return;
-     }
- 
-     if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{12,}$/.test(trimmedPassword)) {
-       this.errorMessage = 'La contraseña debe tener al menos 12 caracteres, una letra mayúscula, un número y un caracter especial';
-       return;
-     }
+    const usernumberInt = parseInt(trimmedUsernumber, 10);
+    if (!((usernumberInt > 90000000 && usernumberInt <= 99999999) || usernumberInt < 100000000)) {
+      this.errorMessage = 'El número de empleado debe ser mayor a 90000000 y menor o igual a 99999999, o menor a 100000000';
+      return;
+    }
+
+    if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{12,}$/.test(trimmedPassword)) {
+      this.errorMessage = 'La contraseña debe tener al menos 12 caracteres, una letra mayúscula, un número y un caracter especial';
+      return;
+    }
 
     if (!/^\d+$/.test(trimmedUsernumber)) {
       this.errorMessage = 'El número de empleado debe contener solo números';
@@ -65,18 +65,22 @@ export class RegisterComponent {
       return;
     }
 
-    this.isRegister = true;
+    // Validación del dominio @coppel
+    if (!/^[a-zA-Z0-9._%+-]+@coppel\.com$/.test(trimmedEmail)) {
+      this.errorMessage = 'El correo electrónico debe ser de la forma nombre@coppel.com';
+      return;
+    }
+
     this.authService.registerUser(trimmedUsernumber, trimmedUsername, trimmedPassword, trimmedEmail)
       .subscribe({
         next: () => {
           this.errorMessage = '';
           setTimeout(() => {
-            this.router.navigate(['/apps/list-apps']);            
+            this.router.navigate(['/apps/list-apps']);
           }, 3000);
         },
-        error: (error : Error) => {
+        error: (error: Error) => {
           this.errorMessage = error.message;
-          this.isRegister = false;
         }
       });
   }
