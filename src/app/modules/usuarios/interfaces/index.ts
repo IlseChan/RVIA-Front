c1558041ed45a2fd7972fbb0f15bac64e26873b3
Brandon Jaimes <b1jaimes89@gmail.com>
2024-09-05 10:13:24 -0600

    `refactor` elementos faltantes

--- src/app/modules/usuarios/interfaces/index.ts
+++ src/app/modules/usuarios/interfaces/index.ts
@@ -1,6 +1,6 @@
 export * from './iduRol.enum';
 export * from './nomRol.enum';
-// export * from './originMethod.interface';
-// export * from './usersData.interface';
+export * from './originMethod.interface';
+export * from './usersData.interface';
 export * from './usuario.interface';
 export * from './usuarioPosition.interface';
---
+++ src/app/modules/usuarios/interfaces/usersData.interface.ts
@@ -0,0 +1,6 @@
+import { Usuario } from "./usuario.interface";
+
+export interface UsersData {
+    data:  Usuario[];
+    total: number;
+}
