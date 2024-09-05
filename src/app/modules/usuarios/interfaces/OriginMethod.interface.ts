61429acc20f3375c4ad753d340c89809a04473ea
Brandon Jaimes <b1jaimes89@gmail.com>
2024-09-05 10:06:27 -0600

    `refactor` eliminando archivos mal nombrados

--- src/app/modules/usuarios/interfaces/OriginMethod.interface.ts
+++
@@ -1,6 +0,0 @@
-export enum OriginMethod {
-    DELETEUSERS = 'DELETEUSERS',
-    GETUSER     = 'GETUSER',
-    GETUSERS    = 'GETUSERS',
-    UPDATEUSER  = 'UPDATEUSER' 
-}
--- src/app/modules/usuarios/interfaces/UsersData.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Usuario } from "./usuario.interface";
-
-export interface UsersData {
-    data:  Usuario[];
-    total: number;
-}
