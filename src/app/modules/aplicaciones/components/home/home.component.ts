f552344d0f04d6831dcb77e26d309bc57bc33f8c
Brandon Jaimes <b1jaimes89@gmail.com>
2024-09-18 13:13:31 -0600

    `styles` Se agrega el cambio de fuente

--- src/app/modules/aplicaciones/components/home/home.component.ts
+++ src/app/modules/aplicaciones/components/home/home.component.ts
@@ -5,18 +5,23 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-  <div class="flex align-items-center p-5 h-full justify-content-center">
+  <div class="flex flex-column align-items-center p-5 h-full justify-content-center">
     <img 
         src="assets/images/logo-rvia.png" 
         alt="rvia-logo" 
         class="img-rvia"
     />
+    <h2 class="text-8xl m-0">Bienvenidos</h2>
   </div>`,
   styles: [`
     .img-rvia {
       max-width: 80%;
       max-height: 80%;
       object-fit: contain;
+    }
+
+    h2{
+      color: var(--rvia-primary-color);
     }
     ` 
   ] 
