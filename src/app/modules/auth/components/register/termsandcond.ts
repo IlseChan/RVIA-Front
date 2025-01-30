84c64845176151213b58931a1ed70c3852c18f22
Brandon <b1jaimes89@gmail.com>
2025-01-30 17:16:28 -0600

    `feat` Se añade terminos y condiciones para registrar usuario

---
+++ src/app/modules/auth/components/register/termsandcond.ts
@@ -0,0 +1,32 @@
+export const termsAndConditions = [
+    {
+        title: '1. Introducción',
+        content: 'Bienvenido/a a <b>RVIA</b> (en adelante, "la Aplicación"). Al registrarse y utilizar la Aplicación, usted acepta cumplir con los siguientes términos y condiciones de uso. Si no está de acuerdo con alguna parte de estos términos, le recomendamos que no utilice la Aplicación.'
+    },
+    {
+        title: '2. Uso de la Aplicación',
+        content: `2.1. La Aplicación ofrece una plataforma para [describir la función principal de la aplicación].\n
+        <br>2.2. Usted es responsable de mantener la confidencialidad de su cuenta y contraseña, así como de todas las actividades que ocurran en su cuenta.\n
+        <br>2.3. Se prohíbe el uso de la Aplicación para cualquier actividad ilegal o no autorizada.`
+    },
+    {
+        title: '3. Responsabilidad sobre el Contenido Subido',
+        content: ` 3.1. La Aplicación permite a los usuarios subir contenido. Aunque tomamos medidas para proteger la integridad de la información subida, el usuario es el único responsable de los archivos, documentos o datos que cargue en la plataforma.
+        <br>3.2. No está permitido subir contenido que contenga llaves de seguridad, credenciales de acceso, información confidencial de terceros u otros elementos de alta seguridad.
+        <br>3.3. La Aplicación no será responsable de cualquier pérdida o uso indebido de la información subida por el usuario.`
+    },
+    {
+        title: ' 4. Privacidad y Seguridad',
+        content: `4.1. Respetamos la privacidad de nuestros usuarios. La información proporcionada se manejará de acuerdo con nuestra Política de Privacidad.
+        <br>4.2. Implementamos medidas de seguridad para proteger la información, pero no garantizamos que la plataforma esté libre de vulnerabilidades o ataques.`
+    },
+    {
+        title: '5. Modificaciones de los Términos',
+        content: `5.1. Nos reservamos el derecho de modificar estos términos en cualquier momento. Se notificará a los usuarios sobre cambios importantes a través de la Aplicación o por correo electrónico.
+        <br>5.2. El uso continuo de la Aplicación después de cualquier modificación implica la aceptación de los nuevos términos.`
+    },
+    {
+        title: '6. Contacto',
+        content: `Si tiene alguna pregunta sobre estos términos y condiciones, puede contactarnos a través de [correo electrónico o canal de soporte].`
+    },
+];
