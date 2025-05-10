79640f143bec4eed1c781514d9c76771c06516f1
Brandon Jaimes <b1jaimes89@gmail.com>
2025-05-10 01:15:29 -0600

    `fix` acceso a usuarios a pagina de ajustes de cuenta propia, valor por defecto para edición de cuenta

--- src/app/modules/usuarios/usuarios.routes.ts
+++ src/app/modules/usuarios/usuarios.routes.ts
@@ -2,18 +2,21 @@ import { Routes } from "@angular/router";
 import { ListUsuariosComponent } from "./components/list-usuarios/list-usuarios.component";
 import { EditUsersPageComponent } from "./components/edit-users-page/edit-users-page.component";
 import { MyAccountPageComponent } from "./components/my-account-page/my-account-page.component";
+import { AdminGuard } from "./guards/adminGuard.guard";
 
 export const usuariosRoutes: Routes = [
     {
         path: '',
         children: [
             { 
-                path: 'list-users', 
+                path: 'list-users',
+                canActivate: [AdminGuard], 
                 component: ListUsuariosComponent,
                 title: 'RVIA - Usuarios' 
             },
             { 
-                path: 'edit/:id', 
+                path: 'edit/:id',
+                canActivate: [AdminGuard], 
                 component: EditUsersPageComponent,
                 title: 'RVIA - Editar usuario'
             },
